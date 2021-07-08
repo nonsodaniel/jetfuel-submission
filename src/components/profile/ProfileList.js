e34276f1b03a196dc9c23bdc8cbb81de56a9094c
nonsodaniel <nonsodaniel07@gmail.com>
2021-07-08 19:34:22 +0100

    Added empty state message

--- src/components/profile/ProfileList.js
+++ src/components/profile/ProfileList.js
@@ -5,20 +5,20 @@ import {ProfileContext} from '../../contexts/ProfileContext'
 const ProfileList = () => {
     const {profile} = useContext(ProfileContext)
     return (
-        <div className="card-body">
-            {
-                profile.length > 0 ? (
-                    profile.map(item =>{
-                        let {id} = item
-                        let tagList = item.tags || [];
-                         return(
-                             <ProfileDetails key={id} profiles={item} tagList={tagList}/>
-                         )
-                     })
-                ) : null
-            }
-    </div>
-    )
+      <div className="card-body">
+        {profile.length > 0 ? (
+          profile.map((item) => {
+            let { id } = item;
+            let tagList = item.tags || [];
+            return (
+              <ProfileDetails key={id} profiles={item} tagList={tagList} />
+            );
+          })
+        ) : (
+          <div className="no-data">No data Available!</div>
+        )}
+      </div>
+    );
 }
 
 export default ProfileList
